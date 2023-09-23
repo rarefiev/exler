72dfe3b50089c90c5925972e3ced69952c394bf4
Roman Ariefiew <rarefiev@gmail.com>
2023-09-23 12:35:21 +0200

    extend utils

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,11 +1,27 @@
 defmodule Utils do
-  def parse do
-    n = parse_line()
-    for _ <- 1..n, do: parse_line()
 
+  def parse_integer() do
+    parse(&Integer.parse/1)
   end
 
-  defp parse_line do
-    IO.gets("") |> IO.chardata_to_string() |> String.trim() |> Integer.parse() |> elem(0)
+  def parse_string() do
+    parse(fn x -> x end)
+  end
+
+  def parse(f) do
+    n = parse_integer_line()
+    for _ <- 1..n, do: parse_line(f)
+  end
+
+  defp parse_integer_line() do
+    parse_line(fn x ->  Integer.parse(x) |> elem(0) end)
+  end
+
+  defp parse_string_line() do
+    parse_line(fn x -> x end)
+  end
+
+  defp parse_line(f) do
+    IO.gets("") |> IO.chardata_to_string() |> String.trim() |> f.()
   end
 end
