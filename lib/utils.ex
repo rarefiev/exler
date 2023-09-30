d9eb5889e5a0ec6de58df76a58890fc12c92b92e
Roman Ariefiew <rarefiev@gmail.com>
2023-09-30 10:44:45 +0200

    [utils] extension.

--- lib/utils.ex
+++ lib/utils.ex
@@ -8,13 +8,17 @@ defmodule Utils do
     parse(fn x -> x end)
   end
 
+  def parse_integer_seq() do
+    parse(&as_intger_seq/1)
+  end
+
   def parse(f) do
     n = parse_integer_line()
     for _ <- 1..n, do: parse_line(f)
   end
 
   defp parse_integer_line() do
-    parse_line(fn x ->  Integer.parse(x) |> elem(0) end)
+    parse_line(&as_integer/1)
   end
 
   defp parse_string_line() do
@@ -24,4 +28,13 @@ defmodule Utils do
   defp parse_line(f) do
     IO.gets("") |> IO.chardata_to_string() |> String.trim() |> f.()
   end
+
+  defp as_integer(x) do
+    Integer.parse(x) |> elem(0)
+  end
+
+  defp as_intger_seq(x) do
+    String.split(x, " ") |> Enum.map(&as_integer/1)
+  end
+
 end
