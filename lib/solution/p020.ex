0703f06894d800eab336dd8fe8758cc0bee5d04c
Roman Ariefiew <rarefiev@gmail.com>
2023-11-26 18:25:19 +0100

    [p020] solved.

--- lib/solution/p020.ex
+++ lib/solution/p020.ex
@@ -5,8 +5,39 @@ defmodule Solution.P020 do
   end
 
   def run(seq) do
-    Enum.map(seq,  &run_one/1)
+    Enum.map(fac_seq(seq),  &sum_of_digits/1)
   end
+
+
+  def sum_of_digits(x) do
+    Integer.to_string(x) |> String.replace("0", "")
+    |> String.split("", trim: true) |> Enum.map_reduce(0, fn x, acc -> {x, acc + (Integer.parse(x) |> elem(0))} end) |> elem(1)
+  end
+
+  def fac_seq(xs) do
+    m = fac_map(xs)
+    Enum.map(xs, fn x -> Map.get(m, x) end)
+  end
+
+  def fac_map(xs) do
+    fac_map(1, 0, Enum.max(xs), Map.new(xs, fn x -> {x, 1} end))
+  end
+
+  def fac_map(_, n, n, m) do
+    m
+  end
+
+  def fac_map(r, i, n, m) do
+    x = i + 1
+    r1 = r * x
+    m1 = if Map.has_key?(m, x) do
+      Map.put(m, x, r1)
+    else
+      m
+    end
+    fac_map(r1, x, n, m1)
+  end
+
 
   def run_one(x) do
     fac(x) |> Integer.to_string() |> String.replace("0", "")
