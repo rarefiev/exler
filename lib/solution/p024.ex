45a9a01483671f3e297566cc4dab609f93310a15
Roman Ariefiew <roman.ariefiew@paypro.pl>
2023-12-22 19:21:13 +0100

    temp.commit2

--- lib/solution/p024.ex
+++ lib/solution/p024.ex
@@ -9,7 +9,7 @@ defmodule Solution.P024 do
   end
 
   def run(xs) do
-    fac_seq = make_factorials() |> IO.inspect(label: "factorials")
+    fac_seq = make_factorials()
     Enum.map(xs, fn x -> run_one(x - 1, fac_seq) end)
   end
 
@@ -18,26 +18,23 @@ defmodule Solution.P024 do
   end
 
   def factorization(n, facs) do
-    Enum.map(facs, fn x -> div(n, x) end) |> IO.inspect(label: "0") |> Enum.slice(1, length(facs) - 2) |> IO.inspect(label: "1") |> Enum.concat([Integer.mod(n, 2), 0]) |> IO.inspect(label: "2")
+    Enum.map(facs, fn x -> div(n, x) end) |> Enum.slice(1, length(facs) - 2) |> Enum.concat([Integer.mod(n, 2), 0])
   end
 
   def run_one(n, facs) do
     xs = factorization(n, facs)
-    res = run_one(xs, @letters, [])
-    Enum.zip([xs, @letters, String.split(res, "", trim: true)]) |> IO.inspect(label: "factrz")
-    res
+    run_one(xs, @letters, [])
   end
 
   def run_one([], orig, dest) do
-    IO.puts("0000 orig: #{orig}, dest: #{dest}")
-    Enum.reverse(orig) |> Enum.concat(dest) |> Enum.reverse() |> Enum.join("") |> IO.inspect(label: "end")
+    Enum.reverse(orig) |> Enum.concat(dest) |> Enum.reverse() |> Enum.join("")
   end
 
   def run_one([x | xs], orig, dest) do
     # IO.puts("x: #{x}, xs: #{xs}, orig: #{orig}, dest: #{dest}")
-    a = Enum.at(orig, x) |> IO.inspect(label: "a")
+    a = Enum.at(orig, x)
     if a != nil do
-      new_orig = Enum.reject(orig, fn z -> z == a end) |> IO.inspect(label: "new orig")
+      new_orig = Enum.reject(orig, fn z -> z == a end)
       run_one(xs, new_orig, [a | dest])
     else
       run_one([], orig, dest)
