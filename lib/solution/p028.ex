76c0c910ec615a8fce7ac47debedfacaca775372
Roman Ariefiew <rarefiev@gmail.com>
2024-01-09 21:42:41 +0100

    [p028] solved.

--- lib/solution/p028.ex
+++ lib/solution/p028.ex
@@ -9,40 +9,11 @@ defmodule Solution.P028 do
   end
 
   def run(xs) do
-    m = tr(Enum.sort(xs), sum_stream())
-    Enum.map(xs, fn x -> Map.get(m, x) end)
+    Enum.map(xs, fn x ->  diag_sum(div(x, 2)) end)
   end
 
-  def f_stream() do
-    Stream.iterate({1, [1]}, &f_step/1)
-  end
-
-  def f_step({n, xs}) do
-    y = Enum.at(xs, -1)
-    xs_1 = Enum.map(1..4, &(&1 * (n + 1) + y))
-    {n + 2, xs_1}
-  end
-
-  def sum_stream() do
-    f_stream() |> Stream.map(fn {i, xs} -> {i, Enum.sum(xs)} end)
-  end
-
-  def tr(xs, stream) do
-    Stream.transform(stream, {0, xs}, &tr_fn/2) |> Map.new()
-  end
-
-  def tr_fn(_, {agg, []}) do
-    {:halt, {nil, nil}}
-  end
-
-
-  def tr_fn({k, v}, {agg, [x | xs] = xs1}) do
-    agg1 = rem(agg + v, @modulo)
-    if k == x do
-      {[{k, agg1}], {agg1, xs}}
-    else
-      {[], {agg1, xs1}}
-    end
+  def diag_sum(n) do
+    rem(1 + 4 * n + div(8 * n * (n + 1) * (2 * n + 1), 3) + 2 * n * (n + 1), @modulo)
   end
 
 end
